27172a3833d0c147cf42e2094047b3ac3b1e1e77
cierravereen24 <cierravereen24@gmail.com>
2019-01-25 16:17:21 -0600

    Classwork

---
+++ graded.py
@@ -0,0 +1,42 @@
+def main():
+    problem1()
+    # problem2
+
+# PROBLEM1
+# Create a function that has a loop
+# Prompt for numbers until the user enters ```q``` to quit
+# If the user doesn't enter ```q```, ask them to input another number
+# When the user enters ```q``` to quit, print the SUM of all numbers entered
+
+# def problem1():
+#     numnum = " "
+#     num = []
+#     while (numnum.lower() != "q"):
+#         numnum = input("Enter some numbers, once you get tired of that type in 'q'.")
+#         numnumnum = numnum
+#         if (numnum == "q"):
+#             print(num[numnumnum])
+
+
+def problem2():
+    num = int(input("Enter a number."))
+    numnum =int(input("Enter another number."))
+
+def do_the_math(num1,num2):
+    def add(num,numnum):
+        sum = num + numnum
+        return sum
+    def sub(num,numnum):
+        difference = num - numnum
+        return difference
+    def multiply(num,numnum):
+        product = num * numnum
+        return product
+    def divide(num, numnum):
+        quotient = num // numnum
+        return quotient
+
+
+
+if __name__ == '__main__':
+    main()
